c311b1a8cfae5b9e28f3f9a7832c569354fc30cd
peterkahumu <muhumukip@gmail.com>
2024-06-18 06:40:21 +0300

    This is my first commit for the django project.

---
+++ Ecommerce/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length = 300)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    imagge = models.ImageField(upload_to= 'products/')
+    availability = models.BooleanField()
+
---
+++ Ecommerce/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('product_list/', views.product_list, name='product_list'),
+    path('product_details/', views.product_details, name='product_details')
+]
